b085d9cd9431dcafc57bcdb1883c99c0b7b1139c
Chris Koying Browet <cbro@semperpax.com>
2017-07-25 11:26:25 +0200

    FIX: adapt patch for kodi

--- cc_shim.py
+++ cc_shim.py
@@ -2,13 +2,14 @@
 
 import re
 import sys
+import os
 from subprocess import call
 
 is_host=False
 
 armv7_flags=["-march=armv7-a", "-mfloat-abi=softfp", "-mfpu=vfpv3-d16", "-Wl,--fix-cortex-a8"]
 
-match_pattern=[r'.*asn1/gen_template.*\.o', r'.*compile_et.*\.o', r'.*print_version.*\.o', r'.*version_128\.o']
+match_pattern=[r'.*asn1/gen_template.*\.o', r'.*compile_et.*\.o', r'.*print_version.*\.o', r'.*version_1..\.o']
 
 match_pattern=[re.compile(pattern) for pattern in match_pattern]
 
@@ -36,4 +37,9 @@ if is_host:
 
   call(cmd, stdout=sys.stdout, stderr=sys.stderr)
 else:
-  call(sys.argv[1:], stdout=sys.stdout, stderr=sys.stderr)
+  cmd=[sys.argv[1]]
+  for arg in os.environ['DROIDCFLAGS'].split():
+    cmd.append(arg)
+  for arg in sys.argv[2:]: 
+    cmd.append(arg)
+  call(cmd, stdout=sys.stdout, stderr=sys.stderr)
